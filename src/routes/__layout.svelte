5b36f138de9353aafef312631ea66b1ea9a9a9d5
Martin Minkov <minkovlmartin@gmail.com>
2021-11-04 14:32:50 -0700

    feat: Add padding to bottom of page on mobile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,7 @@
   import '../app.css';
 </script>
 
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-16">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-16 mb-28 sm:mb-6">
   <div class="max-w-3xl mx-auto">
     <slot />
   </div>
