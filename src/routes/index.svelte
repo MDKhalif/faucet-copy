88ba3f7495d716b002354e08fa1553228d16feb6
Martin Minkov <minkovlmartin@gmail.com>
2021-11-04 18:07:13 -0700

    fix: Add more spacing for the subhead between title and form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,12 +7,12 @@
   <div class="flex flex-col items-center justify-center">
     <img src={MinaLogo} alt="Mina Logo" />
     <h2
-      class="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+      class="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl mt-20"
     >
       Mina Testnet Faucet
     </h2>
   </div>
-  <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+  <p class="mt-20 max-w-3xl mx-auto text-center text-xl text-gray-500">
     Enter a Mina address to receive Testnet funds. Testnet tokens have no value,
     but are useful for experimenting on Mina Testnets.
   </p>
