7f35ad0e137d12fb101cfb4f9490270887e46b3b
Martin Minkov <minkovlmartin@gmail.com>
2021-11-07 11:42:17 -0800

    chore: Add comments to client validateAddress

--- src/lib/utils/validateAddress.js
+++ src/lib/utils/validateAddress.js
@@ -1,3 +1,10 @@
-export default function (address) {
+/**
+ * Checks if the user specified Mina address is valid using a regex check.
+ * Check if the address starts with `B62`, only has alphanumeric characters,
+ * and has a length of 55.
+ * @param {string} address - A Mina address
+ * @returns {boolean} True if valid otherwise false
+ */
+export default (address) => {
   return /^(B62){1}[0-9a-zA-Z]{52}$/i.test(address);
-}
+};
