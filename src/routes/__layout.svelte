6cd8dd4cec58fa2267a79994eab3e5394924a9cc
Martin Minkov <minkovlmartin@gmail.com>
2021-11-17 12:33:34 -0800

    feat: Make max width smaller

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div
-  class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-16 h-screen1.5 sm:h-screen sm:mb-6"
+  class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 mt-16 h-screen1.5 sm:h-screen sm:mb-6"
 >
-  <div class="max-w-7xl mx-auto">
+  <div class="max-w-5xl mx-auto">
     <slot />
   </div>
 </div>
